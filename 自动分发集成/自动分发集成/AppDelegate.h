babec5961d72be0c419fab757056bfb2f5774df3
sharecoding <sharecoding0629@163.com>
2016-03-12 22:36:23 +0800

    xxx

---
+++ 自动分发集成/自动分发集成/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  自动分发集成
+//
+//  Created by victor on 16/3/12.
+//  Copyright © 2016年 Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 自动分发集成/自动分发集成/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  自动分发集成
+//
+//  Created by victor on 16/3/12.
+//  Copyright © 2016年 Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
